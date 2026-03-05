5b10c095d402025062112211f970ffa86e6df966
kocaturkerhan <kocaturkerhan@gmail.com>
2026-03-05 15:33:27 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:33:04.520Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:33:26.887Z
+Commit: Fix CSS styling bug on mobile devices
