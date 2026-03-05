291a8a3d3da2b178b1577b2489a5cc96f7738605
kocaturkerhan <kocaturkerhan@gmail.com>
2026-03-05 15:33:55 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:33:22.920Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T12:33:55.437Z
+Commit: Patch security vulnerability in authentication
