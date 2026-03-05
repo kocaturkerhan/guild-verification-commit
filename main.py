695669b6bfaffbbed8b51b86eff5e2c606bde518
kocaturkerhan <kocaturkerhan@gmail.com>
2026-03-05 15:32:47 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T12:32:32.690Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-05T12:32:46.770Z
+Commit: Fix memory leak in event handlers
